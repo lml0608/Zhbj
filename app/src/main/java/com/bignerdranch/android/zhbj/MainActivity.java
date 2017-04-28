0a1a70cac6b9ec2d0a7718deab1fe870ca2fe7c8
liubin001 <183773928@qq.com>
2017-04-28 08:55:27 +0800

    qwe

--- app/src/main/java/com/bignerdranch/android/zhbj/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/zhbj/MainActivity.java
@@ -2,15 +2,32 @@ package com.bignerdranch.android.zhbj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
+/**
+ * 引入silidingmenu库
+ */
+public class MainActivity extends SlidingFragmentActivity {
+
+    private static final String TAG = "MainActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //去掉标题栏。,在setContentView(R.layout.activity_guide);之前调用
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
 
+        setBehindContentView(R.layout.left_menu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        slidingMenu.setBehindOffset(200);//屏幕预留200像素宽度
 
     }
 }
